5ddb966b26b376ce38989749dd20720728b91e5e
vijayaBommisetty <vijayalakshmi.6506@gmail.com>
2023-03-29 21:30:08 +0530

    modified api

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -16,8 +16,45 @@ const Quiz = () => {
 
     const fetchData = async () => {
       try {
-        const {data} = await axios.get(`http://localhost:7000/quiz/${selectedNumberOfQuestions}`);
-        setQuestions(data.data);
+      const questionsData =  [
+      {
+        "question_id": 1,
+        "question": "What famous Roman general and statesman was assassinated on the Ides of March in 44 BC?",
+        "answer_index": 0,
+        "choices": ["Julius Caesar", "Augustus", "Nero", "Caligula"],
+        "hint": "He was also a renowned military commander.",
+      },
+      {
+        "question_id": 2,
+        "question": "What was the name of the ancient Roman currency?",
+        "answer_index": 1,
+        "choices": ["Sestertius", "Denarius", "Aureus", "As"],
+        "hint": "It was a silver coin.",
+      },
+      {
+        "question_id": 3,
+        "question": "What famous building in Rome was originally built as a stadium for chariot races and other public events?",
+        "answer_index": 1,
+        "choices": ["The Colosseum", "Circus Maximus", "Pantheon", "Forum Romanum"],
+        "hint": "It could hold up to 250,000 spectators.",
+      },
+      {
+        "question_id": 4,
+        "question": "What famous Roman emperor built a wall in northern Britain to protect against Scottish tribes?",
+        "answer_index": 0,
+        "choices": ["Hadrian", "Constantine", "Trajan", "Augustus"],
+        "hint": "The wall is named after him.",
+      },
+      {
+        "question_id": 5,
+        "question": "What was the name of the first Roman emperor?",
+        "answer_index": 3,
+        "choices": ["Julius Caesar", "Tiberius", "Caligula", "Augustus"],
+        "hint": "He was the nephew of Julius Caesar.",
+      }]
+       // const {data} = await axios.get(`http://localhost:7000/quiz/${selectedNumberOfQuestions}`);
+        //setQuestions(data.data);
+        setQuestions(questionsData);
       } catch (error) {
         console.error(error);
       }
